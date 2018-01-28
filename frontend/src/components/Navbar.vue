2fd0b1f89282c08c2dfa11256ef19e0baa8464b7
Kr Shubham <shubhamsinha086@gmail.com>
2018-01-29 02:17:49 +0530

    navbar initiated

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app class="grey lighten-2">
+    <v-toolbar class="orange darken-1">
+      <v-toolbar-title class="white--text">ReviewApp</v-toolbar-title>
+      <v-toolbar-items>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
